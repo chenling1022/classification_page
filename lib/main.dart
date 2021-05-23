add7bbd508c3e0679cd00b2b8a19466d578999d9
Chenling CUI <cuichenling2@gmail.com>
2021-05-23 12:03:57 +0800

    classification page

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 void main() {
   runApp(MyHomePage());
@@ -31,14 +32,44 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   //TODO load model files
-  loadModelFiles() async {}
+  loadModelFiles() async {
+    String res = await Tflite.loadModel(
+        model: "assets/model_unquant.tflite",
+        labels: "assets/labels.txt",
+        numThreads: 1, // defaults to 1
+        isAsset:
+            true, // defaults to true, set to false to load resources outside assets
+        useGpuDelegate:
+            false // defaults to false, set to true to use GPU delegate
+        );
+  }
 
   //run inference and show results
-  doImageClassification() async {}
+  doImageClassification() async {
+    var recognitions = await Tflite.runModelOnImage(
+        path: _image.path, // required
+        imageMean: 0.0, // defaults to 117.0
+        imageStd: 255.0, // defaults to 1.0
+        numResults: 5, // defaults to 5
+        threshold: 0.2, // defaults to 0.1
+        asynch: true // defaults to true
+        );
+    if (recognitions != null) {
+      recognitions.forEach((element) {
+        result += element["label"] +
+            "  " +
+            (element["confidence"] as double).toStringAsFixed(2) +
+            "\n";
+      });
+      setState(() {
+        result;
+      });
+    }
+  }
 
   _imgFromCamera() async {
     PickedFile pickedFile =
-    await imagePicker.getImage(source: ImageSource.camera);
+        await imagePicker.getImage(source: ImageSource.camera);
     _image = File(pickedFile.path);
     setState(() {
       _image;
@@ -48,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _imgFromGallery() async {
     PickedFile pickedFile =
-    await imagePicker.getImage(source: ImageSource.gallery);
+        await imagePicker.getImage(source: ImageSource.gallery);
     _image = File(pickedFile.path);
     setState(() {
       _image;
@@ -56,15 +87,21 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+            appBar: AppBar(
+              title: Text("Local plants in Singapore"),
+              centerTitle: true,
+              backgroundColor: Colors.green,
+            ),
             body: Container(
               decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage('images/img2.jpg'), fit: BoxFit.cover),
+                    image: NetworkImage(
+                        'https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/p-386-felix-mg-9918-l_1.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=f0dfdd21998f2632a109ce6df509dc71'),
+                    fit: BoxFit.cover),
               ),
               child: Column(
                 children: [
@@ -84,26 +121,26 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       ]),
                       Center(
-                        child: FlatButton(
+                        child: TextButton(
                           onPressed: _imgFromGallery,
                           onLongPress: _imgFromCamera,
                           child: Container(
                             margin: EdgeInsets.only(top: 5),
                             child: _image != null
                                 ? Image.file(
-                              _image,
-                              width: 133,
-                              height: 198,
-                              fit: BoxFit.fill,
-                            )
+                                    _image,
+                                    width: 133,
+                                    height: 198,
+                                    fit: BoxFit.fill,
+                                  )
                                 : Container(
-                              width: 140,
-                              height: 190,
-                              child: Icon(
-                                Icons.camera_alt,
-                                color: Colors.grey[800],
-                              ),
-                            ),
+                                    width: 140,
+                                    height: 190,
+                                    child: Icon(
+                                      Icons.camera_alt,
+                                      color: Colors.grey[800],
+                                    ),
+                                  ),
                           ),
                         ),
                       ),
@@ -114,9 +151,19 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Text(
                       '$result',
                       textAlign: TextAlign.center,
-                      style: TextStyle(fontFamily: 'finger_paint', fontSize: 36),
+                      style: TextStyle(
+                          fontFamily: 'RobotoMono',
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green.withOpacity(1.0)),
                     ),
                   ),
+                  Container(
+                      margin: EdgeInsets.only(bottom: 100),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF1D1E33),
+                        borderRadius: BorderRadius.circular(10.0),
+                      )),
                 ],
               ),
             )));
